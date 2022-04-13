e212da2d81566f9348b4f3f3b269bf1dea2f9dc6
Ahmed Yasen <ahmedyasen8@gmail.com>
2022-04-13 16:46:15 +0200

    restructured

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,7 +1,17 @@
-mod identity;
+mod ed25519_identity;
 mod message;
+mod sr25519_identity;
 mod twin;
 
-pub use identity::*;
 pub use message::*;
 pub use twin::*;
+
+pub use ed25519_identity::*;
+pub use sr25519_identity::*;
+
+use anyhow::Result;
+
+pub trait Identity: Clone {
+    fn id(&self) -> u64;
+    fn sign(&self, msg: Message) -> Result<Message>;
+}
