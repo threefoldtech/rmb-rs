acbc3ef49cedbfa29db440f9c88e3dbbc9d08e88
Ahmed Yasen <ahmedyasen8@gmail.com>
2022-05-11 16:55:35 +0200

    add prefix and ttl to RedisCache

--- src/cache/redis.rs
+++ src/cache/redis.rs
@@ -15,16 +15,33 @@ use serde::{de::DeserializeOwned, Serialize, Serializer};
 // how_to_init
 // let manager = RedisConnectionManager::new(url)?;
 // let pool = Pool::builder().build(manager).await?;
+// RedisCache::new(pool, "twin", 10); //10 seconds
 //
 
 #[derive(Clone)]
-pub struct RedisCache {
+pub struct RedisCache<P>
+where
+    P: Into<String>,
+{
     pool: Pool<RedisConnectionManager>,
+    prefix: P,
+    ttl: usize,
 }
 
-impl RedisCache {
-    pub async fn new(pool: Pool<RedisConnectionManager>) -> Result<Self> {
-        Ok(Self { pool })
+impl<P> RedisCache<P>
+where
+    P: Into<String>,
+{
+    pub async fn new(
+        pool: Pool<RedisConnectionManager>,
+        prefix: P,
+        ttl_sec: usize,
+    ) -> Result<Self> {
+        Ok(Self {
+            pool,
+            prefix,
+            ttl: ttl_sec,
+        })
     }
 
     async fn get_connection(&self) -> Result<PooledConnection<'_, RedisConnectionManager>> {
@@ -32,23 +49,27 @@ impl RedisCache {
             .pool
             .get()
             .await
-            .context("unable to retrive a redis connection from the pool")?;
+            .context("unable to retrieve a redis connection from the pool")?;
 
         Ok(conn)
     }
 }
 
 #[async_trait]
-impl<T> Cache<T> for RedisCache
+impl<T, P> Cache<T> for RedisCache<P>
 where
     T: Serialize + DeserializeOwned + Send + Sync + 'static,
+    P: Into<String> + Send + Sync + Clone,
 {
     async fn set<S: ToString + Send + Sync>(&self, key: S, obj: T) -> Result<()> {
         let mut conn = self.get_connection().await?;
         let obj = serde_json::to_vec(&obj).context("unable to serialize twin object for redis")?;
+        let mut key = format!("{:?}.{}", self.prefix.clone().into(), key.to_string());
         cmd("SET")
-            .arg(key.to_string())
+            .arg(key)
             .arg(obj)
+            .arg("EX")
+            .arg(self.ttl)
             .query_async(&mut *conn)
             .await?;
 
@@ -65,7 +86,7 @@ where
         match ret {
             Some(val) => {
                 let ret: T = serde_json::from_slice(&val)
-                    .context("unable to deserialze redis value to twin object")?;
+                    .context("unable to deserialize redis value to twin object")?;
 
                 Ok(Some(ret))
             }
