7c70d879e14ed86e065864ec7de7feff1f108680
Ahmed Yasen <ahmedyasen8@gmail.com>
2022-05-12 15:34:23 +0200

    - using RwLock instead of Mutex - remove non-necessary members from MemoryCache

--- src/cache/memory.rs
+++ src/cache/memory.rs
@@ -1,6 +1,6 @@
 use std::{borrow::BorrowMut, collections::HashMap, time::Duration};
 
-use tokio::sync::Mutex;
+use tokio::sync::RwLock;
 
 use super::Cache;
 use crate::twin::Twin;
@@ -9,17 +9,13 @@ use async_trait::async_trait;
 use serde_json::Value;
 
 pub struct MemCache<V> {
-    mem: Mutex<HashMap<String, V>>,
-    prefix: String,
-    ttl: Duration,
+    mem: RwLock<HashMap<String, V>>,
 }
 
 impl<V> MemCache<V> {
     pub async fn new<P: Into<String>>(prefix: P, ttl: Duration) -> Self {
         Self {
-            mem: Mutex::new(HashMap::new()),
-            prefix: prefix.into(),
-            ttl,
+            mem: RwLock::new(HashMap::new()),
         }
     }
 }
@@ -30,14 +26,14 @@ where
     T: Clone + Send + Sync + 'static,
 {
     async fn set<K: ToString + Send + Sync>(&self, key: K, obj: T) -> Result<()> {
-        let mut mem = self.mem.lock().await;
+        let mut mem = self.mem.write().await;
         mem.insert(key.to_string(), obj);
 
         Ok(())
     }
 
     async fn get<K: ToString + Send + Sync>(&self, key: K) -> Result<Option<T>> {
-        let mem = self.mem.lock().await;
+        let mem = self.mem.read().await;
         match mem.get(&key.to_string()) {
             None => Ok(None),
             Some(v) => Ok(Some(v.clone())),
