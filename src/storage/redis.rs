580f18931bc85d0f12cba170a2a3ca1b9c145790
Abdelrahman Ghanem <abom.jdev@gmail.com>
2022-11-17 16:52:05 +0200

    keep the original message proxy flag just...
    this would allow older clients to work without setting proxy=false

--- src/storage/redis.rs
+++ src/storage/redis.rs
@@ -373,19 +373,15 @@ mod tests {
             id: String::from(id),
             command: String::from("test.get"),
             expiration: 300,
-            data: String::from(""),
-            tag: None,
             source: 1,
             destination: vec![4],
             reply: String::from("de31075e-9af4-4933-b107-c36887d0c0f0"),
             retry: 2,
-            schema: String::from(""),
             timestamp: SystemTime::now()
                 .duration_since(SystemTime::UNIX_EPOCH)
                 .unwrap()
                 .as_secs(),
-            error: None,
-            signature: None,
+            ..Message::default()
         };
 
         conn.lpush(&queue, &msg).await?;
