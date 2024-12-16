963611e89fc37a4afb2869a7472fed3150623002
Sameh Abouel-saad <samehabouelsaad@gmail.com>
2024-12-16 19:53:02 +0200

    style: convert double quotes (") to single quotes (') for static strings that don't need variable expansion

--- tools/rmb_tester/test_live_nodes.sh
+++ tools/rmb_tester/test_live_nodes.sh
@@ -10,32 +10,32 @@ case $1 in
 esac
 
 if [ -z "$MNEMONIC" ]; then
-  echo "MNEMONIC is not set"
-  echo "Please set the MNEMONIC environment variable"
-  echo "Example: MNEMONIC='...' ./test_live_nodes.sh <NETWORK-ALIAS>"
+  echo 'MNEMONIC is not set'
+  echo 'Please set the MNEMONIC environment variable'
+  echo 'Example: MNEMONIC="..." ./test_live_nodes.sh <NETWORK-ALIAS>'
   exit 1
 fi
 
-if [[ "$1" == "main" ]]; then
-  SUBSTRATE_URL="wss://tfchain.grid.tf:443"
-  RELAY_URL="wss://relay.grid.tf"
+if [[ "$1" == 'main' ]]; then
+  SUBSTRATE_URL='wss://tfchain.grid.tf:443'
+  RELAY_URL='wss://relay.grid.tf'
 else
   SUBSTRATE_URL="wss://tfchain.$1.grid.tf:443"
   RELAY_URL="wss://relay.$1.grid.tf"
 fi
-RMB_LOG_FILE="./rmb-peer.log"
+RMB_LOG_FILE='./rmb-peer.log'
 TIMEOUT="${TIMEOUT:-60}"
 RMB_BIN="${RMB_BIN:-../../target/x86_64-unknown-linux-musl/release/rmb-peer}"
 VERBOSE="${VERBOSE:-false}"
 cleanup() {
   set +e
-  echo "cleaning up initiated"
+  echo 'cleaning up initiated'
   if [ -n "$VIRTUAL_ENV" ]; then
-    echo "deactivating virtual environment"
+    echo 'deactivating virtual environment'
     deactivate
   fi
   # close redis-server
-  echo "closing redis-server ..."
+  echo 'closing redis-server ...'
   redis-cli -p 6379 shutdown
   jlist=$(jobs -pr)
   plist=$(ps --ppid $$ | awk '/[0-9]/{print $1}' | grep -v -E "^$$|^$(pgrep -f 'ps')|^$(pgrep -f 'awk')|^$(pgrep -f 'grep')$")
@@ -53,18 +53,18 @@ trap cleanup SIGHUP	SIGINT SIGQUIT SIGABRT SIGTERM
 echo "starting live nodes rmb test version $(git describe --tags)"
 # start redis in backgroud and skip errors in case alreday running
 set +e
-echo "redis-server starting ..."
+echo 'redis-server starting ...'
 
 redis-server --port 6379 > /dev/null 2>&1 &
 sleep 3
 # clear all databases
-echo "Removes all keys in Redis"
+echo 'Removes all keys in Redis'
 redis-cli -p 6379 FLUSHALL
 set -e
 
 # ensure that RMB is not already running
 if pgrep -x $(basename "$RMB_BIN") > /dev/null; then
-    echo "Another instance of rmb-peer is already running. Killing..."
+    echo 'Another instance of rmb-peer is already running. Killing...'
     pkill -x $(basename "$RMB_BIN")
 fi
 
