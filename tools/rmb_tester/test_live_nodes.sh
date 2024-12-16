d538658d8254aba1865e3799dc7d9fbe72ff3f27
Sameh Abouel-saad <samehabouelsaad@gmail.com>
2024-12-16 20:09:16 +0200

    improve logging and debug output in the script

--- tools/rmb_tester/test_live_nodes.sh
+++ tools/rmb_tester/test_live_nodes.sh
@@ -27,39 +27,48 @@ RMB_LOG_FILE='./rmb-peer.log'
 TIMEOUT="${TIMEOUT:-60}"
 RMB_BIN="${RMB_BIN:-../../target/x86_64-unknown-linux-musl/release/rmb-peer}"
 VERBOSE="${VERBOSE:-false}"
+DEBUG="${DEBUG:-false}"
+
 cleanup() {
   set +e
-  echo 'cleaning up initiated'
+  debug 'cleaning up initiated'
   if [ -n "$VIRTUAL_ENV" ]; then
-    echo 'deactivating virtual environment'
+    debug 'deactivating virtual environment'
     deactivate
   fi
   # close redis-server
-  echo 'closing redis-server ...'
+  debug 'closing redis-server ...'
   redis-cli -p 6379 shutdown
   jlist=$(jobs -pr)
   plist=$(ps --ppid $$ | awk '/[0-9]/{print $1}' | grep -v -E "^$$|^$(pgrep -f 'ps')|^$(pgrep -f 'awk')|^$(pgrep -f 'grep')$")
   pids=${jlist:-$plist}
   if [ -n "$pids" ]; then
-    echo "stop rmb-peer and all bash managed jobs"
+    debug "stop rmb-peer and all bash managed jobs"
     kill $pids
   else
-    echo "All jobs in this bash session have completed or stoped, so there are none left to clean up."
+    debug "All jobs in this bash session have completed or stoped, so there are none left to clean up."
   fi
+}
+
+debug() {
+    if [[ "$DEBUG" == "true" ]]; then
+        echo "$@"
+    fi
 }
 
 trap cleanup SIGHUP	SIGINT SIGQUIT SIGABRT SIGTERM
 
-echo "starting live nodes rmb test version $(git describe --tags)"
+echo "starting live nodes rmb test script version $(git describe --tags)"
+echo "network used: $1net"
 # start redis in backgroud and skip errors in case alreday running
 set +e
-echo 'redis-server starting ...'
+debug 'redis-server starting ...'
 
 redis-server --port 6379 > /dev/null 2>&1 &
 sleep 3
 # clear all databases
-echo 'Removes all keys in Redis'
-redis-cli -p 6379 FLUSHALL
+debug 'Removes all keys in Redis'
+redis-cli -p 6379 FLUSHALL > /dev/null 2>&1 &
 set -e
 
 # ensure that RMB is not already running
@@ -72,7 +81,7 @@ fi
 MNEMONIC="${MNEMONIC#"${MNEMONIC%%[![:space:]]*}"}"; MNEMONIC="${MNEMONIC%"${MNEMONIC##*[![:space:]]}"}"
 
 # start rmb in background
-echo "rmb-peer starting ("$1"net).."
+debug "rmb-peer starting ($1net).."
 $RMB_BIN -m "$MNEMONIC" --substrate "$SUBSTRATE_URL" --relay "$RELAY_URL" --redis "redis://localhost:6379" --debug &> $RMB_LOG_FILE &
 
 # wait till peer establish connection to a relay
@@ -84,7 +93,7 @@ fi
 
 # start rmb_tester
 source venv/bin/activate
-echo "rmb_tester starting .."
+debug "rmb_tester starting .."
 python3 ./rmb_tester.py -d $(./scripts/twins.sh --likely-up $1) -c "zos.system.version" -t "$TIMEOUT" -e "$TIMEOUT" $(if [[ "$VERBOSE" == "false" ]]; then echo "--short"; fi)
 
 cleanup
